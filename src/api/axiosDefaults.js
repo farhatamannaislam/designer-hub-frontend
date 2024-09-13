ea30e0389ac9583c0497a274cd0c7b2a031b13f4
Farha Islam <farha.islam@students.codeinstitute.net>
2024-09-13 11:20:06 +0000

    Add: Initial Authentication and SignUp Form

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://designerhubbackend-ebd8c03488fb.herokuapp.com/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
