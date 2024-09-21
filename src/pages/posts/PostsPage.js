5cc7ef337201563e08072ad27a07205bce576fa3
Farha Islam <farha.islam@students.codeinstitute.net>
2024-09-21 09:58:11 +0000

    Update: search options

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -70,7 +70,7 @@ function PostsPage({ message, filter = "" }) {
             onChange={(event) => setQuery(event.target.value)}
             type="text"
             className="mr-sm-2"
-            placeholder="Search posts"
+            placeholder="Search posts by authorname, category, title, content"
           />
         </Form>
 
